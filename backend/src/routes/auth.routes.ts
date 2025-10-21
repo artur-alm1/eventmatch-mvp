1b6a0a7da513e8babcbf5f2bc1ef1c40434fcffa
Artur Luna Marinho <alm6@cin.ufpe.br>
2025-10-21 07:02:10 -0300

    ajuste em auth.routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,13 @@
+// src/routes/auth.routes.ts
+
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller';
 
 const router = Router();
 const authController = new AuthController();
 
-router.post('/register', (req, res) => authController.register(req, res));
-router.post('/login', (req, res) => authController.login(req, res));
+// Rotas de autenticação
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 export default router;
